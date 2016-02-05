ba9bc446ba7415e1d6bb4a5a15fba48f62ff35e1
migege <lzw.whu@gmail.com>
2016-02-06 00:55:18 +0800

    valid rss feed

--- controllers/feed.go
+++ controllers/feed.go
@@ -19,15 +19,17 @@ func (this *FeedController) RSS() {
 	for _, post := range posts {
 		rss_post := models.NewRSSPost(post)
 		rss_post.PostLink = blogUrl + rss_post.PostLink
+		rss_post.GUID = rss_post.PostLink
 		rss_posts = append(rss_posts, rss_post)
 	}
 	rss := models.NewRSSFeed()
 	channel := models.NewRSSChannel()
-	channel.PubDate = time.Now()
+	channel.PubDate = time.Now().Format("Mon, 02 Jan 2006 15:04:05 MST")
 	channel.ChannelTitle = blogTitle
 	channel.ChannelLink = blogUrl
 	channel.ChannelDesc = blogDesc
 	channel.RSSPosts = rss_posts
+	channel.Self = models.NewRSSSelf(blogUrl + "/rss")
 	rss.Channel = channel
 
 	this.Data["xml"] = rss
--- models/feed.go
+++ models/feed.go
@@ -6,32 +6,49 @@ import (
 )
 
 type RSSPost struct {
-	XMLName     xml.Name  `xml:"item"`
-	PostTitle   string    `xml:"title"`
-	PostLink    string    `xml:"link"`
-	PostContent string    `xml:"description"`
-	PostTime    time.Time `xml:"pubDate"`
+	XMLName     xml.Name `xml:"item"`
+	PostTitle   string   `xml:"title"`
+	PostLink    string   `xml:"link"`
+	PostContent string   `xml:"description"`
+	PostTime    string   `xml:"pubDate"`
+	GUID        string   `xml:"guid"`
 }
 
 func NewRSSPost(post *Post) *RSSPost {
 	loc, _ := time.LoadLocation("Asia/Shanghai")
-	time, _ := time.ParseInLocation("2006-01-02 15:04:05", post.PostTime, loc)
+	post_time, _ := time.ParseInLocation("2006-01-02 15:04:05", post.PostTime, loc)
 	return &RSSPost{
 		PostTitle:   post.PostTitle,
 		PostLink:    post.PostLink(),
 		PostContent: post.PostContent,
-		PostTime:    time,
+		PostTime:    post_time.Format("Mon, 02 Jan 2006 15:04:05 MST"),
+	}
+}
+
+type RSSSelf struct {
+	XMLName xml.Name `xml:"atom:link"`
+	Href    string   `xml:"href,attr"`
+	Rel     string   `xml:"rel,attr"`
+	Type    string   `xml:"type,attr"`
+}
+
+func NewRSSSelf(href string) *RSSSelf {
+	return &RSSSelf{
+		Href: href,
+		Rel:  "self",
+		Type: "application/rss+xml",
 	}
 }
 
 type RSSChannel struct {
-	XMLName       xml.Name  `xml:"channel"`
-	ChannelTitle  string    `xml:"title"`
-	ChannelLink   string    `xml:"link"`
-	ChannelDesc   string    `xml:"description"`
-	ChannelEditor string    `xml:"managingEditor"`
-	PubDate       time.Time `xml:"pubDate"`
-	RSSPosts      []*RSSPost
+	XMLName      xml.Name `xml:"channel"`
+	ChannelTitle string   `xml:"title"`
+	ChannelLink  string   `xml:"link"`
+	ChannelDesc  string   `xml:"description"`
+	//ChannelEditor string   `xml:"managingEditor"`
+	PubDate  string `xml:"pubDate"`
+	Self     *RSSSelf
+	RSSPosts []*RSSPost
 }
 
 func NewRSSChannel() *RSSChannel {
@@ -41,11 +58,13 @@ func NewRSSChannel() *RSSChannel {
 type RSSFeed struct {
 	XMLName xml.Name `xml:"rss"`
 	Version string   `xml:"version,attr"`
+	NS      string   `xml:"xmlns:atom,attr"`
 	Channel *RSSChannel
 }
 
 func NewRSSFeed() *RSSFeed {
 	return &RSSFeed{
 		Version: "2.0",
+		NS:      "http://www.w3.org/2005/Atom",
 	}
 }

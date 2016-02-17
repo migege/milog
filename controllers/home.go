c79544ffb6e223320239496ecfeddf47ee5cf458
migege <lzw.whu@gmail.com>
2016-02-18 00:43:41 +0800

    show post views on home

--- controllers/home.go
+++ controllers/home.go
@@ -16,11 +16,22 @@ func (this *MainController) Get() {
 	}
 	paginator := pagination.SetPaginator(this.Ctx, postsPerPage, post_count)
 
-	posts, err := models.NewPostModel().Offset("", nil, "-PostId", paginator.Offset(), postsPerPage)
+	posts, err := models.NewPostModel().Offset("", nil, "-PostId", paginator.Offset(), postsPerPage, false, true, true)
 	if err != nil {
 		panic(err)
 	}
 	this.Data["Posts"] = posts
+
+	views := make(map[int]int)
+	for _, post := range posts {
+		for _, view := range post.PostViews {
+			if view.ViewedBy == "human" {
+				views[post.PostId] = view.Views
+				break
+			}
+		}
+	}
+	this.Data["Views"] = views
 
 	latest_comments, err := models.NewCommentModel().Latest(10)
 	if err == nil {
